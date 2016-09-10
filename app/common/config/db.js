e4c25f8ca4b3db0c6aa27ce93c6c1b9e3feb97b4
beAce <18817834467@163.com>
2016-09-10 17:56:23 +0800

    添加依赖

---
+++ app/common/config/db.js
@@ -0,0 +1,18 @@
+
+      "use strict";
+      exports.__esModule = true;
+      exports.default = {
+    "type": "mysql",
+    "adapter": {
+        "mysql": {
+            "host": "localhost",
+            "port": "3306",
+            "database": "beaceblog",
+            "user": "root",
+            "password": "",
+            "prefix": "fk_",
+            "type": "mysql"
+        }
+    }
+}
+    
